37fe9eb5ce6b4fff54317030b8f6a69f4e6250fa
Michael Gall <michael@wakeless.net>
2011-09-15 02:38:22 +1000

    Puting the deface in the right spot

--- lib/spree_digital_hooks.rb
+++ lib/spree_digital_hooks.rb
@@ -1,7 +1,7 @@
 class SpreeDigitalHooks < Spree::ThemeSupport::HookListener
   Deface::Override.new(:virtual_path => "admin/shared/_product_tabs",
                      :name => "converted_admin_product_tabs_986577829",
-                     :insert_after => "[data-hook='admin_product_tabs'], #admin_product_tabs[data-hook]",
+                     :insert_bottom => "[data-hook='admin_product_tabs'], #admin_product_tabs[data-hook]",
                      :text => "    <li<%== ' class=\"active\"' if current == \"Digital Versions\" %>>
       <%= link_to t(\"digital_versions\"), admin_product_digitals_path(@product) %>
     </li>
