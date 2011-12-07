7fc4fd58d5651bdc48946b5233fe9a6d915ed03f
Michael Gall <michael@wakeless.net>
2011-12-08 01:42:35 +1100

    Using resource controller and namespace

---
+++ app/models/spree/digital_link.rb
@@ -0,0 +1,29 @@
+module Spree
+  class DigitalLink < ActiveRecord::Base
+    
+    belongs_to :digital
+    belongs_to :line_item
+  
+    before_validation :set_defaults, :on => :create
+    
+    # Can this link stil be used? It is valid if it's less than 24 hours old and was not accessed more than 3 times
+    def authorizable?
+      self.created_at > 1.day.ago and self.access_counter < 3
+    end
+    
+    # This method should be called when a download is initiated.
+    # It returns +true+ or +false+ depending on whether the authorization is granted.
+    def authorize!
+      authorizable? && increment!(:access_counter) ? true : false
+    end
+    
+    private
+    
+    # Populating the secret automatically and zero'ing the access_counter (otherwise it might turn out to be NULL)
+    def set_defaults
+      self.secret = SecureRandom.hex(15)
+      self.access_counter = 0
+    end
+  
+  end
+end
