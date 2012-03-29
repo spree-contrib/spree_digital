7f729bd402f801696a0bdc57a5d73e8644e5cac0
Michael Bianco <iloveitaly@gmail.com>
2012-03-28 21:51:16 -0400

    Forgot to save merged changed. Minor deface fix

--- app/overrides/add_digital_versions_to_admin_product_tabs.rb
+++ app/overrides/add_digital_versions_to_admin_product_tabs.rb
@@ -1,4 +1,4 @@
-Deface::Override.new(:virtual_path => "admin/shared/_product_tabs",
+Deface::Override.new(:virtual_path => "spree/admin/shared/_product_tabs",
                      :name => "add_digital_versions_to_admin_product_tabs",
                      :insert_bottom => "[data-hook='admin_product_tabs'], #admin_product_tabs[data-hook]",
                      :text => "    <li<%== ' class=\"active\"' if current == \"Digital Versions\" %>>
--- lib/spree_digital.rb
+++ lib/spree_digital.rb
@@ -1,20 +1,2 @@
 require 'spree_core'
-<<<<<<< HEAD
-
-module SpreeDigital
-  class Engine < Rails::Engine
-
-    config.autoload_paths += %W(#{config.root}/lib)
-
-    def self.activate
-      Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
-        Rails.env.production? ? require(c) : load(c)
-      end
-    end
-
-    config.to_prepare &method(:activate).to_proc
-  end
-end
-=======
 require 'spree_digital/engine'
->>>>>>> 9b919de... Modified to fit spree 0.70 extension
