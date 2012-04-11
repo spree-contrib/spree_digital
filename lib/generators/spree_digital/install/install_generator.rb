ab049fb4bb8a96f4220cddebfa7542a4d10e1db8
Michael Bianco <iloveitaly@gmail.com>
2012-04-11 16:24:56 -0400

    Removing unneeded CSS + JS assets

--- lib/generators/spree_digital/install/install_generator.rb
+++ lib/generators/spree_digital/install/install_generator.rb
@@ -2,17 +2,17 @@ module SpreeDigital
   module Generators
     class InstallGenerator < Rails::Generators::Base
       
-      def add_javascripts
-        append_file "app/assets/javascripts/store/all.js", "//= require store/spree_digital\n"
-        append_file "app/assets/javascripts/admin/all.js", "//= require admin/spree_digital\n"
-      end
+      # def add_javascripts
+      #   append_file "app/assets/javascripts/store/all.js", "//= require store/spree_digital\n"
+      #   append_file "app/assets/javascripts/admin/all.js", "//= require admin/spree_digital\n"
+      # end
       
-      def add_stylesheets
-        inject_into_file "app/assets/stylesheets/store/all.css", " *= require store/spree_digital\n",
-                         :before => /\*\//, :verbose => true
-        inject_into_file "app/assets/stylesheets/admin/all.css", " *= require admin/spree_digital\n",
-                         :before => /\*\//, :verbose => true
-      end
+      # def add_stylesheets
+      #   inject_into_file "app/assets/stylesheets/store/all.css", " *= require store/spree_digital\n",
+      #                    :before => /\*\//, :verbose => true
+      #   inject_into_file "app/assets/stylesheets/admin/all.css", " *= require admin/spree_digital\n",
+      #                    :before => /\*\//, :verbose => true
+      # end
       
       def add_migrations
         run 'bundle exec rake railties:install:migrations FROM=spree_digital'
