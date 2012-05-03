c5f0c79a03741a004418ffe80ea623eb069dbeb1
Michael Bianco <iloveitaly@gmail.com>
2012-05-03 10:59:32 -0400

    Present message to user when no shipping methods are available

--- app/overrides/modify_shipping_options_display.rb
+++ app/overrides/modify_shipping_options_display.rb
@@ -20,7 +20,9 @@ Deface::Override.new(:virtual_path => "spree/checkout/_delivery",
   <%== t 'digital_shipping', :email => @order.email %> (<%= number_to_currency @order.digital_shipping_method[:cost] %>)
 </label>
 <% else %>
-<% @order.rate_hash.each do |shipping_method| %>
+<% filtered_rate_hash = @order.rate_hash.select { |m| !(@order.digital_shipping_method && shipping_method[:id] == @order.digital_shipping_method[:id]) } %>
+<% if filtered_rate_hash.count > 0 %>
+<% filtered_rate_hash.each do |shipping_method| %>
 <% next if @order.digital_shipping_method && shipping_method[:id] == @order.digital_shipping_method[:id] %>
   <label>
     <%= radio_button(:order, :shipping_method_id, shipping_method[:id]) %>
@@ -30,6 +32,9 @@ Deface::Override.new(:virtual_path => "spree/checkout/_delivery",
     <%= shipping_method[:name] %> <%= number_to_currency shipping_method[:cost] %>
     <% end %>
   </label>
+<% end %>
+<% else %>
+<%= t :no_shipping_methods %>
 <% end %>
 <% end %>
 })
