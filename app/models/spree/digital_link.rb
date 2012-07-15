192fa68291491fabb90d35e52f02a0a54e8874bc
Chris Lindensmith <lindensmith@github>
2012-07-15 13:41:53 -0700

    Add preferences options for allowing/disallowing logged-in downloads Add limit preference to logged-in downloads to limit abuse - should update failure messages to reflect failure mode - should add pref settings to admin panels

--- app/models/spree/digital_link.rb
+++ app/models/spree/digital_link.rb
@@ -11,8 +11,10 @@ module Spree
     
     # Can this link stil be used? It is valid if it's less than 24 hours old and was not accessed more than 3 times
     # The part after the or allows a logged in user to download for as long as the order still displays in their account
+    # extended_click_limit sets an additional limit on the number of downloads for logged in users to reduce abuse of that feature
     def authorizable?
-      (self.created_at > Spree::DigitalConfiguration[:authorized_days].day.ago and self.access_counter < Spree::DigitalConfiguration[:authorized_clicks]) or (self.line_item.order.user == Spree::User.current)
+      (self.created_at > Spree::DigitalConfiguration[:authorized_days].day.ago and self.access_counter < Spree::DigitalConfiguration[:authorized_clicks]) \
+        or (self.line_item.order.user == Spree::User.current and Spree::DigitalConfiguration[:extended_download] and self.access_counter < Spree::DigitalConfiguration[:extended_click_limit])
     end
     
     # This method should be called when a download is initiated.
