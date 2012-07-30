c2cf4df7b37ffed92416de5382585684032f2c43
Chris Lindensmith <lindensmith@earthlink.net>
2012-07-30 13:33:26 -0700

    Revert "bring extended download into up to date master"
    This reverts commit c20b5176247ca77d4cdcec20c75f2d0f28b0f439, reversing
    changes made to 38002486cd563f8547f0465eedb2c60a18330860.

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -14,11 +14,7 @@ Spree::Variant.class_eval do
   # We need to delete the Digital manually here as soon as the Variant is nullified.
   # Otherwise you'll have orphan Digitals (and their attached files!) associated with unused Variants. 
   def destroy_digital
-<<<<<<< HEAD
     digitals.map &:destroy
-=======
-    digitals.destroy
->>>>>>> 845d6e4790f1f19fb1f60430f95a555d4f2d3d92
   end
 
 end
