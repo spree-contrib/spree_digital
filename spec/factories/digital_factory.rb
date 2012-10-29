4dc2d9d54f41df54ec04867e217f952a64933947
Michael Bianco <iloveitaly@gmail.com>
2012-10-29 13:05:35 -0400

    Minor improvement to digital factory

--- spec/factories/digital_factory.rb
+++ spec/factories/digital_factory.rb
@@ -2,5 +2,8 @@ FactoryGirl.define do
   factory :digital, :class => Spree::Digital do |f|
     # TODO good to assign variant association if no association is manually defined
     # f.variant { |p| p.association(:variant) }
+
+    attachment_content_type 'application/octet-stream'
+    attachment_file_name 'a_great_book.epub'
   end
 end
