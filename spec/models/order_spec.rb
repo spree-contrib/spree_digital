031000ab1de350d3784667de3bf492c2a49f3f23
Peter M. Goldstein <peter.m.goldstein@gmail.com>
2013-08-22 12:43:20 -0700

    Replace add_variant with contents.add to eliminate deprecation warnings.

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,23 +1,22 @@
 require 'spec_helper'
 
 describe Spree::Order do
-  context "#add_variant" do
+  context "contents.add" do
     it "should add digital Variants of quantity 1 to an order" do
       order = FactoryGirl.create(:order)
-      order.add_variant variant1 = FactoryGirl.create(:variant, :digitals => [FactoryGirl.create(:digital)])
-      order.add_variant variant2 = FactoryGirl.create(:variant, :digitals => [FactoryGirl.create(:digital)])
-      order.add_variant variant3 = FactoryGirl.create(:variant, :digitals => [FactoryGirl.create(:digital)])
-      order.line_items.first.variant.should == variant1
-      order.line_items.second.variant.should == variant2
-      order.line_items.third.variant.should == variant3
+      variants = 3.times.map { create(:variant, :digitals => [FactoryGirl.create(:digital)]) }
+      variants.each { |v| order.contents.add(v, 1) }
+      order.line_items.first.variant.should == variants[0]
+      order.line_items.second.variant.should == variants[1]
+      order.line_items.third.variant.should == variants[2]
     end
     
     it "should handle quantity higher than 1 when adding one specific digital Variant" do
       order = FactoryGirl.create(:order)
       digital_variant = FactoryGirl.create(:variant, :digitals => [FactoryGirl.create(:digital)])
-      order.add_variant digital_variant, 3
+      order.contents.add digital_variant, 3
       order.line_items.first.quantity.should == 3
-      order.add_variant digital_variant, 2
+      order.contents.add digital_variant, 2
       order.line_items.first.quantity.should == 5
     end
   end
@@ -26,21 +25,21 @@ describe Spree::Order do
     it "should understand that all products are digital" do
       order = FactoryGirl.create(:order)
       3.times do
-        order.add_variant FactoryGirl.create(:variant, :digitals => [FactoryGirl.create(:digital)])
+        order.contents.add create(:variant, :digitals => [FactoryGirl.create(:digital)]), 1
       end
       order.digital?.should be_true
-      order.add_variant FactoryGirl.create(:variant, :digitals => [FactoryGirl.create(:digital)]), 4
+      order.contents.add FactoryGirl.create(:variant, :digitals => [FactoryGirl.create(:digital)]), 4
       order.digital?.should be_true
     end
     
     it "should understand that not all products are digital" do
       order = FactoryGirl.create(:order)
       3.times do
-        order.add_variant FactoryGirl.create(:variant, :digitals => [FactoryGirl.create(:digital)])
+        order.contents.add FactoryGirl.create(:variant, :digitals => [FactoryGirl.create(:digital)]), 1
       end
-      order.add_variant FactoryGirl.create(:variant) # this is the analog product
+      order.contents.add FactoryGirl.create(:variant), 1 # this is the analog product
       order.digital?.should be_false
-      order.add_variant FactoryGirl.create(:variant, :digitals => [FactoryGirl.create(:digital)]), 4
+      order.contents.add FactoryGirl.create(:variant, :digitals => [FactoryGirl.create(:digital)]), 4
       order.digital?.should be_false
     end
   end
