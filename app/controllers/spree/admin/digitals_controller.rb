bd0d20a2fa460f2811a598cd058ceb42557b9cc4
Grant Warman <grant.warman@gmail.com>
2013-09-19 15:40:55 -0400

    Removed strong params

--- app/controllers/spree/admin/digitals_controller.rb
+++ app/controllers/spree/admin/digitals_controller.rb
@@ -6,13 +6,5 @@ module Spree
       def location_after_save
         spree.admin_product_digitals_path(@product)
       end
-
-      def permitted_resource_params
-        params.require(:digital).permit(permitted_digital_attributes)
-      end
-
-      def permitted_digital_attributes
-        [:variant_id, :attachment] 
-      end
   end
 end
