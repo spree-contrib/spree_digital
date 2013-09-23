062d0245f1f5fd09d40c64491609c89b27fe9c8a
Grant Warman <grant.warman@gmail.com>
2013-09-23 12:18:58 -0400

    Added basic test coverage of s3 in digitals_controller

--- app/controllers/spree/digitals_controller.rb
+++ app/controllers/spree/digitals_controller.rb
@@ -1,31 +1,51 @@
 module Spree
   class DigitalsController < Spree::StoreController
     ssl_required :show
+    rescue_from ActiveRecord::RecordNotFound, with: :resource_not_found
 
     def show
       link = DigitalLink.find_by_secret(params[:secret])
 
-      if link.present? and link.digital.attachment.present?
-        attachment = link.digital.attachment
-
-
+      if attachment.present? 
 
         # don't authorize the link unless the file exists
         # the logger error will help track down customer issues easier
-        if attachment.exists?
-          if link.authorize!
+        if attachment_is_file? 
+          if digital_link.authorize!
             if Spree::Config[:use_s3]
               redirect_to attachment.expiring_url(Spree::DigitalConfiguration[:s3_expiration_seconds]) and return
             else
               send_file attachment.path, :filename => attachment.original_filename, :type => attachment.content_type and return
             end
           end
+
         else
           Rails.logger.error "Missing Digital Item: #{attachment.path}"
         end
       end
 
       render :unauthorized
+    end
+
+
+    private
+
+    def attachment_is_file?
+      return attachment.exists?
+    end
+
+    def digital_link
+      @link ||= DigitalLink.find_by_secret(params[:secret])
+      raise ActiveRecord::RecordNotFound if @link.nil?
+      @link
+    end
+
+    def attachment
+      @attachment ||= digital_link.digital.try(:attachment) if digital_link.present?
+    end
+
+    def resource_not_found
+      head status: 404
     end
 
   end
