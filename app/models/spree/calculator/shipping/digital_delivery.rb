29de92145cf5cb237ed96ae471faa7b1e0da42ea
Jeff Dutil <jdutil@burlingtonwebapps.com>
2013-10-11 18:30:54 -0400

    Fix calculator.

--- app/models/spree/calculator/shipping/digital_delivery.rb
+++ app/models/spree/calculator/shipping/digital_delivery.rb
@@ -1,8 +1,9 @@
 # https://github.com/spree/spree/issues/1439
-require_dependency 'spree/calculator'
+require_dependency 'spree/shipping_calculator'
 
 module Spree
-    class Calculator::Shipping::DigitalDelivery < Calculator::Shipping::FlatRate
+  module Calculator::Shipping
+    class DigitalDelivery < Spree::Calculator::Shipping::FlatRate
       def self.description
         I18n.t(:digital_delivery)
       end
@@ -15,4 +16,5 @@ module Spree
         package.contents.all? { |content| content.variant.digital? }
       end
     end
+  end
 end
