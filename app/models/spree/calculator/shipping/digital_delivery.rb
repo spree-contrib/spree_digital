4fa102d1f11e689221b2331587f0a281b1d4e659
Grant Warman <grant.warman@gmail.com>
2013-10-11 17:33:58 -0400

    Modified DigitalDelivery so it inherits from Spree::ShippingCalculator. Fixes #48

---
+++ app/models/spree/calculator/shipping/digital_delivery.rb
@@ -0,0 +1,18 @@
+# https://github.com/spree/spree/issues/1439
+require_dependency 'spree/calculator'
+
+module Spree
+    class Calculator::Shipping::DigitalDelivery < Calculator::Shipping::FlatRate
+      def self.description
+        I18n.t(:digital_delivery)
+      end
+
+      def compute(object=nil)
+        self.preferred_amount
+      end
+
+      def available?(package)
+        package.contents.all? { |content| content.variant.digital? }
+      end
+    end
+end
