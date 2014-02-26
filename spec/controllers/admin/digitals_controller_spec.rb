1dd1f153cdd5a8dc3bcd44cfcee3d77f282585cb
Dave Kiss <hello@davekiss.com>
2014-02-26 11:05:46 -0600

    Changed permalink to slug in accordance with spree switching to using the friendly_id gem

--- spec/controllers/admin/digitals_controller_spec.rb
+++ spec/controllers/admin/digitals_controller_spec.rb
@@ -17,7 +17,7 @@ describe Spree::Admin::DigitalsController do
 
       it "should display an empty page when no digitals exist" do
         variants_without_digitals
-        spree_get :index, product_id: product.permalink
+        spree_get :index, product_id: product.slug
       end
 
       it "should display list of digitals when they exist" do
@@ -28,7 +28,7 @@ describe Spree::Admin::DigitalsController do
     context "without non-master variants" do
 
       it "should display an empty page when the master variant is not digital" do
-        spree_get :index, product_id: product.permalink
+        spree_get :index, product_id: product.slug
         response.code.should == "200"
         response.body.should include("This product has no variants")
         response.body.should_not include('A digital version of this product currently exists')
@@ -36,7 +36,7 @@ describe Spree::Admin::DigitalsController do
 
       it "should display the variant details when the master is digital" do
         @digital = create :digital, :variant => product.master
-        spree_get :index, product_id: product.permalink
+        spree_get :index, product_id: product.slug
         response.code.should == "200"
         response.body.should include('A digital version of this product currently exists')
       end
@@ -50,7 +50,7 @@ describe Spree::Admin::DigitalsController do
 
       it 'creates a digital associated with the variant and product' do
         lambda {
-          spree_post :create, product_id: product.permalink, 
+          spree_post :create, product_id: product.slug, 
                               digital: { variant_id: variant.id, 
                                          attachment: upload_image('thinking-cat.jpg') }
           response.should redirect_to(spree.admin_product_digitals_path(product))
@@ -66,7 +66,7 @@ describe Spree::Admin::DigitalsController do
     context 'for a digital and product that exist' do
       it 'deletes the associated digital' do
         lambda {
-          spree_delete :destroy, product_id: product.permalink, id: digital.id
+          spree_delete :destroy, product_id: product.slug, id: digital.id
           response.should redirect_to(spree.admin_product_digitals_path(product))
         }.should change(Spree::Digital, :count).by(-1)
       end
