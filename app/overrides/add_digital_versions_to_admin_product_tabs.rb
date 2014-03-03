2be633b8fb277108aa8a34d7458aa5799cb88382
Dave Kiss <hello@davekiss.com>
2014-03-03 14:18:13 -0600

    Updated translations for https://github.com/spree/spree/commit/d4ff64bf865dfddf900d04ec8b9acc163d1e6838

--- app/overrides/add_digital_versions_to_admin_product_tabs.rb
+++ app/overrides/add_digital_versions_to_admin_product_tabs.rb
@@ -2,7 +2,7 @@ Deface::Override.new(:virtual_path => "spree/admin/shared/_product_tabs",
                      :name => "add_digital_versions_to_admin_product_tabs",
                      :insert_bottom => "[data-hook='admin_product_tabs'], #admin_product_tabs[data-hook]",
                      :text => "    <li<%== ' class=\"active\"' if current == \"Digital Versions\" %>>
-      <%= link_to t(\"digital_versions\"), admin_product_digitals_path(@product) %>
+      <%= link_to Spree.t(:digital_versions, scope: 'digitals'), admin_product_digitals_path(@product) %>
     </li>
 ",
                      :disabled => false)
