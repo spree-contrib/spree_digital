c65fe61e480347e4ad60105b7486e09b7c26cce3
Dave Kiss <hello@davekiss.com>
2014-03-03 17:02:15 -0600

    Added valid content types to spec

--- spec/factories/digital_factory.rb
+++ spec/factories/digital_factory.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :digital, :class => Spree::Digital do |f|
     f.variant { |p| p.association(:variant) }
 
-    attachment_content_type 'application/octet-stream'
+    attachment_content_type 'application/epub+zip'
     attachment_file_name "#{SecureRandom.hex(5)}.epub"
   end
 end
