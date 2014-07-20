b763107e0cd7120ee5163b0eec4c69904730b120
Jeff Dutil <JDutil@BurlingtonWebApps.com>
2014-07-20 00:33:51 -0400

    Fix validation. Fixes #69

--- app/models/spree/digital.rb
+++ app/models/spree/digital.rb
@@ -1,9 +1,10 @@
 module Spree
   class Digital < ActiveRecord::Base
     belongs_to :variant
-    has_many :digital_links, :dependent => :destroy
+    has_many :digital_links, dependent: :destroy
 
-    has_attached_file :attachment, :path => ":rails_root/private/digitals/:id/:basename.:extension"
+    has_attached_file :attachment, path: ":rails_root/private/digitals/:id/:basename.:extension"
+    do_not_validate_attachment_file_type :attachment
 
     if Paperclip::Attachment.default_options[:storage] == :s3
       attachment_definitions[:attachment][:s3_permissions] = :private
