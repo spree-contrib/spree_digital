3cd169ca33a2e0a229c02b10ddefc088cc0e3ec9
Jacob Spizziri <jacob.spizziri@gmail.com>
2015-04-16 14:26:51 -0500

    redirect on validation error

--- app/controllers/spree/admin/digitals_controller.rb
+++ app/controllers/spree/admin/digitals_controller.rb
@@ -3,6 +3,24 @@ module Spree
     class DigitalsController < ResourceController
       belongs_to "spree/product", :find_by => :slug
       
+      def create
+        invoke_callbacks(:create, :before)
+        @object.attributes = permitted_resource_params
+
+        if @object.valid?
+          super
+        else
+          invoke_callbacks(:create, :fails)
+          respond_with(@object) do |format|
+            format.html do
+              flash.now[:error] = @object.errors.full_messages.join(", ")
+              render action: 'index'
+            end
+            format.js { render layout: false }
+          end
+        end
+      end
+
       protected
         def location_after_save
           spree.admin_product_digitals_path(@product)
