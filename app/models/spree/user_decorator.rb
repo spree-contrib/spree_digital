39a7154eee8e860fc039d88e527c9152821c2290
Tania <tania@efaber.net>
2015-06-18 16:23:19 +0200

    Change user.digital_signature to include user email

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -2,7 +2,7 @@ Spree.user_class.class_eval do
   has_many :digital_links, -> {order(:updated_at)}, foreign_key: :user_id, class_name: 'Spree::DigitalLink'
 
   def digital_signature
-    I18n.t("spree.digitals.copy_for", name: self.full_name)
+    I18n.t("spree.digitals.copy_for", name: self.full_name, email: self.email)
   end
 
   def full_name
