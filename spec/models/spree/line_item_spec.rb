d15239e36573e1d52cbe823bd9f5c57c5ab8ae54
Dave Kiss <dave@davekiss.com>
2015-09-15 11:19:01 -0400

    Allow a line item to report as digital if the variant's master is digital

--- spec/models/spree/line_item_spec.rb
+++ spec/models/spree/line_item_spec.rb
@@ -2,22 +2,29 @@ require 'spec_helper'
 
 RSpec.describe Spree::LineItem do
 
+  let!(:digital_variant) { create(:variant, :digitals => [create(:digital)]) }
+  let!(:master_digital_variant) { create(:on_demand_master_variant, :digitals => [create(:digital)]) }
+  let!(:line_item) { create(:line_item, :variant => digital_variant) }
+  let!(:links) { digital_variant.digitals.first.digital_links }
+
+  context "#digital?" do
+    it "reports as digital if either the master variant or selected variant has digitals" do
+      expect(create(:variant)).not_to be_digital
+      expect(create(:on_demand_master_variant)).not_to be_digital
+
+      expect(digital_variant).to be_digital
+      expect(master_digital_variant).to be_digital
+    end
+  end
+
   context "#save" do
     it "should create one link for a single digital Variant" do
-      digital_variant = create(:variant, :digitals => [create(:digital)])
-      line_item = create(:line_item, :variant => digital_variant)
       links = digital_variant.digitals.first.digital_links
       expect(links.to_a.size).to eq(1)
       expect(links.first.line_item).to eq(line_item)
     end
 
     it "should create a link for each quantity of a digital Variant, even when quantity changes later" do
-      digital_variant = create(:variant, :digitals => [create(:digital)])
-      line_item = create(:line_item, :variant => digital_variant, :quantity => 5)
-      links = digital_variant.digitals.first.digital_links
-      expect(links.to_a.size).to eq(5)
-      links.each { |link| expect(link.line_item).to eq(line_item) }
-
       # quantity update
       line_item.quantity = 8
       line_item.save
@@ -33,9 +40,6 @@ RSpec.describe Spree::LineItem do
 
   context "#destroy" do
     it "should destroy associated links when destroyed" do
-      digital_variant = create(:variant, :digitals => [create(:digital)])
-      line_item = create(:line_item, :variant => digital_variant)
-      links = digital_variant.digitals.first.digital_links
       expect(links.to_a.size).to eq(1)
       expect(links.first.line_item).to eq(line_item)
       expect {
