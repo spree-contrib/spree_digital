bb706008b0ab8c382f1bd3fb5cbbca3882672be2
Tania <tania@efaber.net>
2017-09-28 13:20:52 +0200

    Sort digital by updated_at desc

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,6 +1,6 @@
 Spree::Product.class_eval do
   has_many :digitals, :through => :variants_including_master
-  scope :digital, -> { joins(:digitals).reorder("spree_digitals.updated_at DESC")}
+  scope :digital, -> { includes(:digitals).reorder("spree_digitals.updated_at DESC").distinct }
 
   def has_paper_or_digital_variants?
     self.variants_including_master.any?{|a| a.has_digital_option?}
