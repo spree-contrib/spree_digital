765d80f36a9655f463a461373abeff7330a97cc1
PSzyszkaSpark <pawel.szyszka@sparksolutions.co>
2019-10-31 06:36:42 +0100

    Changes class_eval to prepend in decorators

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,25 +1,28 @@
-Spree::Order.class_eval do
-  # all products are digital
-  def digital?
-    line_items.all? { |item| item.digital? }
-  end
-  
-  def some_digital?
-    line_items.any? { |item| item.digital? }
-  end
+module Spree
+  module OrderDecorator
+    # all products are digital
+    def digital?
+      line_items.all? { |item| item.digital? }
+    end
 
-  def digital_line_items
-    line_items.select(&:digital?)
-  end
+    def some_digital?
+      line_items.any? { |item| item.digital? }
+    end
 
-  def digital_links
-    digital_line_items.map(&:digital_links).flatten
-  end
+    def digital_line_items
+      line_items.select(&:digital?)
+    end
 
-  def reset_digital_links!
-    digital_links.each do |digital_link|
-      digital_link.reset!
+    def digital_links
+      digital_line_items.map(&:digital_links).flatten
+    end
+
+    def reset_digital_links!
+      digital_links.each do |digital_link|
+        digital_link.reset!
+      end
     end
   end
-
 end
+
+::Spree::Order.prepend(Spree::OrderDecorator)
