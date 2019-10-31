3baa2e4db3c23b4ad5c5a43cc6d3eb4ab8d37081
PSzyszkaSpark <pawel.szyszka@sparksolutions.co>
2019-10-31 06:40:05 +0100

    Adds curved brackets in factories to remove deprecation warning

--- spec/factories/digital_factory.rb
+++ spec/factories/digital_factory.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :digital, :class => Spree::Digital do |f|
     f.variant { |p| p.association(:variant) }
 
-    attachment_content_type 'application/octet-stream'
-    attachment_file_name "#{SecureRandom.hex(5)}.epub"
+    attachment_content_type { 'application/octet-stream' }
+    attachment_file_name    { "#{SecureRandom.hex(5)}.epub" }
   end
 end
