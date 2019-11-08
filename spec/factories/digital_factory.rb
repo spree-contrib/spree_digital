526f43fc8660595059e59d691c7abacada310b6d
PSzyszkaSpark <pawel.szyszka@sparksolutions.co>
2019-11-08 09:54:23 +0100

    Corrects factories

--- spec/factories/digital_factory.rb
+++ spec/factories/digital_factory.rb
@@ -4,5 +4,13 @@ FactoryBot.define do
 
     attachment_content_type { 'application/octet-stream' }
     attachment_file_name    { "#{SecureRandom.hex(5)}.epub" }
+    attachment_file_size    { 10 }
+
+    after(:build) do |digital|
+      digital.attachment.attach(
+        io: StringIO.new(digital.attachment_content_type),
+        filename: digital.attachment_file_name
+      )
+    end
   end
 end
