9243941f0a5b4a2f11917cdff30981c7269efb82
Damian Legawiec <damian@sparksolutions.co>
2021-01-21 17:46:18 +0100

    Remove empty assets

--- lib/generators/spree_digital/install/install_generator.rb
+++ lib/generators/spree_digital/install/install_generator.rb
@@ -3,18 +3,6 @@ module SpreeDigital
     class InstallGenerator < Rails::Generators::Base
       class_option :auto_run_migrations, :type => :boolean, :default => false
 
-      def add_javascripts
-        append_file "vendor/assets/javascripts/spree/frontend/all.js", "//= require spree/frontend/spree_digital\n"
-        append_file "vendor/assets/javascripts/spree/backend/all.js", "//= require spree/backend/spree_digital\n"
-      end
-
-      def add_stylesheets
-        inject_into_file "vendor/assets/stylesheets/spree/frontend/all.css", " *= require spree/frontend/spree_digital\n",
-                         :before => /\*\//, :verbose => true
-        inject_into_file "vendor/assets/stylesheets/spree/backend/all.css", " *= require spree/backend/spree_digital\n",
-                         :before => /\*\//, :verbose => true
-      end
-
       def add_migrations
         run 'bundle exec rake railties:install:migrations FROM=spree_digital'
       end
